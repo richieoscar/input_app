33a3c6c5570d2cda114e83699fbf1a84642c9c61
richieoscar <oscaranyiam94@gmail.com>
2021-08-18 15:09:58 +0100

    task2

---
+++ lib/task2/print_name.dart
@@ -0,0 +1,11 @@
+void printName() {
+  String fullName = "Oscar Anyiam";
+  print("My Name is, " + fullName);
+  print("");
+  print("Task 2 completed");
+}
+
+ //run code here
+// main(){
+// printName();
+// }
